7f7dea0c3f0bcb2fbf6fa39eef6996f20ec3c6e6
Dawid Janiga <dawidjaniga@gmail.com>
2019-02-05 13:29:35 +0100

    Init slides

--- presentation/index.js
+++ presentation/index.js
@@ -44,60 +44,33 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={['slide']}
         transitionDuration={500}
+        progress="bar"
         theme={theme}
       >
         <Slide transition={['zoom']} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            VR w Webie
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" fit bold>
-            open the presentation/index.js file to get started
+            Wprowadzenie, status, przykłady
           </Text>
         </Slide>
-        <Slide bgColor="secondary">
-          <Image src={images.formidagon} width={800} />
-        </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
+        <Slide transition={['fade']} bgColor="tertiary" notes="na chłopski rozum">
           <Heading size={2} textColor="secondary">
-            Heading 2
+            Czym jest WebVR?
           </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
+          <Text textColor="primary">Biblioteką do tworzenie wirtualnej rzeczywiście w przeglądarce.</Text>
+          <Text textColor="primary">Działa na HTC Vive, Oculus Rift i przeglądrakch mobilnych (Android, iOS)</Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
+            <ListItem>A</ListItem>
+            <ListItem>B</ListItem>
             <ListItem>Item 3</ListItem>
             <ListItem>Item 4</ListItem>
           </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite margin="10px 0 0 30px">Author</Cite>
-          </BlockQuote>
         </Slide>
         <Slide>
           <Image src={images.goodWork} width={500} />
